293622a2cd7cfa7a879ed103694b5bae5badfa0d
felipesuri <felipe@felipesuri.com>
2023-07-05 06:40:56 -0300

    feat: :wrench: Update users service and tests

--- src/users/controller.spec.ts
+++ src/users/controller.spec.ts
@@ -1,24 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { User } from '@prisma/client'
 
 import { UsersController } from './controller'
 import { UsersService } from './service'
 
 import { PrismaModule } from '@/prisma/module'
+import { PrismaService } from '@/prisma/service'
+
+const db = {}
 
 describe('UsersController', () => {
   let controller: UsersController
+  let prisma: PrismaService
+  let user: User
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [PrismaModule],
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: PrismaService,
+          useValue: db,
+        },
+      ],
     }).compile()
 
     controller = module.get<UsersController>(UsersController)
+    prisma = module.get<PrismaService>(PrismaService)
   })
 
   it('should be defined', () => {
     expect(controller).toBeDefined()
+  })
+
+  describe('root', () => {
+    it('should create a user', () => {
+      expect(controller.create({ name: 'John Doe ' })).toBe(undefined)
+    })
   })
 })
--- src/users/service.spec.ts
+++ src/users/service.spec.ts
@@ -3,6 +3,19 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { UsersService } from './service'
 
 import { PrismaModule } from '@/prisma/module'
+import { PrismaService } from '@/prisma/service'
+
+const JohnDoe = { name: 'John Doe' }
+
+const users = [{ ...JohnDoe }, { name: 'Geralt of Rivia' }, { name: 'Yennefer' }]
+
+const db = {
+  user: {
+    findMany: jest.fn().mockReturnValue(users),
+    findUnique: jest.fn().mockReturnValue(JohnDoe),
+    create: jest.fn().mockReturnValue(JohnDoe),
+  },
+}
 
 describe('UsersService', () => {
   let service: UsersService
@@ -10,7 +23,13 @@ describe('UsersService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [PrismaModule],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: PrismaService,
+          useValue: db,
+        },
+      ],
     }).compile()
 
     service = module.get<UsersService>(UsersService)
@@ -18,5 +37,26 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  describe('create user', () => {
+    it('should successfully insert a user', async () => {
+      const user = await service.create(JohnDoe)
+      expect(user).toEqual(JohnDoe)
+    })
+  })
+
+  describe('find many', () => {
+    it('should return an array of users', async () => {
+      const manyUsers = await service.findAll()
+      expect(manyUsers).toEqual(manyUsers)
+    })
+  })
+
+  describe('find one users', () => {
+    it('should find one users by id', async () => {
+      const user = await service.findOne('a mock uuid')
+      expect(user).toEqual(JohnDoe)
+    })
   })
 })
