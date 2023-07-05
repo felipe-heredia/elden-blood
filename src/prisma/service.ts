72e0b00770dcb00f67f28deba2d0be01b54b6faf
felipesuri <felipe.heredia@starcompany.com.br>
2023-07-05 11:52:19 -0300

    feat: :card_file_box: Implement softDelete

--- src/prisma/service.ts
+++ src/prisma/service.ts
@@ -1,15 +1,33 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common'
-import { PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect()
+    this.$use(this.softDeleteMiddleware)
   }
 
   async enableShutdownHooks(app: INestApplication) {
     this.$on('beforeExit', async () => {
       await app.close()
     })
+  }
+
+  softDeleteMiddleware: Prisma.Middleware = async (params, next) => {
+    if (params.action === 'delete') {
+      return next({
+        ...params,
+        action: 'update',
+        args: {
+          ...params.args,
+          data: {
+            deletedAt: new Date(),
+          },
+        },
+      })
+    }
+
+    return next(params)
   }
 }
