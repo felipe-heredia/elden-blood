2026e0695c5782febf4aecc51245d423a03ca0b9
felipesuri <felipe@felipesuri.com>
2023-07-06 06:20:01 -0300

    feat: add store to app module

--- src/app/module.ts
+++ src/app/module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common'
 import { AppController } from './controller'
 import { AppService } from './service'
 
+import { StoreModule } from '@/store/module'
 import { UsersModule } from '@/users/module'
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, StoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
