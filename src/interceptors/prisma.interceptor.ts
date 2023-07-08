6cc5946d84da392f18d592194f3a0ef125bca891
felipesuri <felipe@felipesuri.com>
2023-07-08 16:49:45 -0300

    feat(prisma): Add interceptor

---
+++ src/interceptors/prisma.interceptor.ts
@@ -0,0 +1,57 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  BadRequestException,
+} from '@nestjs/common'
+import { Prisma } from '@prisma/client'
+import { Observable } from 'rxjs'
+import { catchError } from 'rxjs/operators'
+
+@Injectable()
+export class PrismaInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      catchError(error => {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+          console.log(error)
+
+          const errorMessage = this.extractErrorMessage(error.message)
+          const errors = this.extractErrors(errorMessage)
+
+          if (errors) {
+            throw new BadRequestException({
+              errors,
+              message: errorMessage,
+            })
+          }
+        }
+
+        throw new BadRequestException(error)
+      })
+    )
+  }
+
+  private extractErrorMessage(fullErrorMessage: string): string {
+    const splitStr = '\nUnique constraint failed on the '
+    return fullErrorMessage.split(splitStr)[1] || fullErrorMessage
+  }
+
+  private extractErrors(errorMessage: string): Record<string, string> | null {
+    const regex = /\((.*?)\)/g
+    const matches = errorMessage.match(regex)
+
+    if (matches) {
+      const fields = matches.map(match => match.replace(/[\(\)]/g, ''))
+      const errors = fields.reduce((acc, field) => {
+        acc[field] = `Unique constraint failed on field: ${field}`
+        return acc
+      }, {})
+
+      return errors
+    }
+
+    return null
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app/module'
+import { PrismaInterceptor } from './interceptors/prisma.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -15,6 +16,8 @@ async function bootstrap() {
       transform: true,
     })
   )
+
+  app.useGlobalInterceptors(new PrismaInterceptor())
 
   await app.listen(3000)
 }
